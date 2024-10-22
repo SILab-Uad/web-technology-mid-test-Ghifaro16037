915b7fb298ec1d9c0b7cb789f71a8184c69598b5
Ghifaro16037 <2300016037@webmail.uad.ac.id>
2024-10-22 11:07:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,66 @@
 
 const generatePassword = (length, options) => {
     // Character sets for password generation
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const specialChars = "!@#$%^&*()";
+    const HurufBesar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const HurufKecil = "abcdefghijklmnopqrstuvwxyz";
+    const Nomor = "0123456789";
+    const KarakterSpesial = "!@#$%^&*()";
+
+    // Create Variabel
+    let characterSet = "";
 
     // TODO: Create a variable for the character set based on selected options
+    if (options.includeUppercase) characterSet += HurufBesar;
+    if (options.includeLowercase) characterSet += HurufKecil;
+    if (options.includeNumbers) characterSet += Nomor;
+    if (options.includeSpecialChars) characterSet += KarakterSpesial;
+
+    // Fallback
+    if (characterSet === "") {
+        alert("Please select at least one option")
+        return "";
+    }
 
     // TODO: Generate the password based on the selected criteria
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomindex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randomindex];
+    }
     return password;
 };
 
 // TODO: Add event listener to the button to call generatePassword and display the output
+document.getElementById('generateBtn').addEventListener('click', () => {
+    const length = parseInt(document.getElementById('length').value, 10);
+    
+    // Validation
+    if (isNaN(length) || length <= 0 ) {
+        alert("Please enter a valid password length")
+        return;
+    }
 
+    const options = {
+        includeUppercase: document.getElementById('includeUppercase').checked,
+        includeLowercase: document.getElementById('includeLowercase').checked,
+        includeNumbers: document.getElementById('includeNumbers').checked,
+        includeSpecialChars: document.getElementById('includeSpecialChars').checked,
+    };
+    
+    const password = generatePassword(length, options);
+    document.getElementById("passwordOutput").value = password;
+
+})
 // BONUS: Implement the copy to clipboard functionality
+document.getElementById('copyBtn').addEventListener('click', () => {
+    const passwordOutput = document.getElementById('passwordOutput').value;
+    if (passwordOutput) {
+        navigator.clipboard.writeText(passwordOutput).then(() => {
+            alert('Password copied to clipboard!');
+        }).catch(err => {
+            console.error('Could not copy text: ', err);
+        });
+    } else {
+        alert('No password to copy!');
+    }
+ });
